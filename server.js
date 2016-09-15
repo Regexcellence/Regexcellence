92992ed34fb0ca9b5a21f101008b1f3c428106cb
Troy Gibb <troy.gibb@gmail.com>
2016-09-14 20:14:05 -0700

    [ BUGFIX ] Fix missing parantheses

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, '/build	')));
 
 app.get('/', (req, res) => {
   console.log('Serving /');
-  res.sendFile(path.join(__dirname, '/build/index.html')
+  res.sendFile(path.join(__dirname, '/build/index.html'))
 });
 
 app.listen(port, function() {console.log(` Listening on http://localhost:${port}/`)});
