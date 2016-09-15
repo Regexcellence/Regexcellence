478ad21ada38400bfba06c7c3d5fb62c54b29903
Troy Gibb <troy.gibb@gmail.com>
2016-09-14 20:29:37 -0700

    [ BUGFIX ] Work on routing for deployment

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.set('port', port);
 
-app.use(express.static(path.join(__dirname, '/build	')));
+//app.use(express.static(path.join(__dirname, '/build')));
 
-app.get('/', (req, res) => {
-  console.log('Serving /');
-  res.sendFile(path.join(__dirname, '/build/index.html'))
+app.get('/*', (req, res) => {
+  console.log('Serving ' + req.url);
+  res.sendFile(path.join(__dirname, `/build/${req.url}`))
 });
 
 app.listen(port, function() {console.log(` Listening on http://localhost:${port}/`)});
