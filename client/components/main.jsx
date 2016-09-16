b9dc669a50a1d39e35b155563d1d650ace76af4c
Dana Li <danagotmail@gmail.com>
2016-09-15 20:03:13 -0700

    [FEATURE] Input | Test | Render Input - Add functionality to test input Test - Add test functionality Render - Add challenge component to page

---
+++ client/components/main.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Challenge from './challenge';
+import Progress from './progress';
+import Lesson from './lesson';
+
+class Main extends Component{
+  render(){
+    return(
+      <div>
+        <Progress />
+        <Lesson />
+        <Challenge />
+      </div>
+    );
+  }
+}
+
+export default Main;
---
+++ client/components/progress.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Progress extends Component{
+  render(){
+    return (
+      <div> Progress </div>
+    );
+  }
+}
+
+export default Progress;
