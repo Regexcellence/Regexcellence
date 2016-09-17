dbe0a5a77b1c5114b475245bb6f24b6dfdf136f6
Troy Gibb <troy.gibb@gmail.com>
2016-09-16 19:33:05 -0700

    [ FEATURE ] Create intial redux implementation

--- client/app.jsx
+++ client/app.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // For creating eventual <Provider /> tag
-// import { Provider } from 'react-redux';
+import { Provider } from 'react-redux';
 // To create 'store' variable
-// import { createStore } from 'redux';
+import { createStore } from 'redux';
+import store from './reducers/index';
 
 import Main from './components/main';
+
+
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <p>Hello Regexcellence</p>
-        <Main />
+        <Provider store={store}>
+        	<Main />
+        </Provider>
       </div>
     );
   }
--- client/reducers/index.jsx
+++ client/reducers/index.jsx
@@ -6,3 +6,30 @@
 // });
 
 // export default allReducers;
+import { createStore } from 'redux';
+
+const initialState = [
+  { id: 1, case: 'abcd', result: null, task: 'Match', expectation: true },
+  { id: 2, case: 'acd', result: null, task: 'Skip', expectation: false },
+  { id: 3, case: 'abcde', result: null, task: 'Match', expectation: true },
+];
+
+/* Action = {
+	type: 'UPDATE-RESULT',
+	newVal: null/true/false,
+	challengeId: INTEGER
+}
+*/
+const reducer = (state = initialState, action) => {
+	if (action.type === 'UPDATE-RESULT') {
+		return state.map((challenge) => {
+			if (action.id === challenge.id) {
+				challenge.id = action.newVal;
+			} 
+			return challenge;
+		});
+	}
+	return state; 
+};
+
+export default createStore(reducer); 
