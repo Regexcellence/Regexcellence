d6e616051e281b32e091a55c8d74b349a8068c04
Benjamin Tran <benjamin.tran25@gmail.com>
2016-09-20 15:47:59 -0700

    [ TEST ] Add test for Challenges component

--- client/controls.jsx
+++ client/controls.jsx
@@ -4,12 +4,13 @@ import Challenges from './components/challenges';
 import Progress from './components/progress';
 
 export default class Controls extends React.Component {
-	render() {
-		return (
-			<div>
+  render() {
+    return (
+      <div>
         <Progress />
-				<Challenges />
-			</div>
-		);
-	}
+        <Lesson />
+        <Challenges />
+      </div>
+    );
+  }
 }
--- spec/first.test.js
+++ spec/first.test.js
@@ -1,36 +1,51 @@
 import React from 'react';
 import { expect } from 'chai';
-import { mount, shallow } from 'enzyme';
-
+import { shallow } from 'enzyme';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import Controls from '../client/controls';
-import Challenge from '../client/components/challenge';
+import Challenge from '../client/containers/challenge';
+import Challenges from '../client/components/challenges';
+import InputRegexValidation from '../client/containers/inputRegexValidation';
+import reducer from '../client/reducers/index';
+
+const finalStore = applyMiddleware(thunk)(createStore);
+const testStore = finalStore(reducer);
 
 describe('<Controls />', () => {
-  it('should have h3 tag', () => {
-    const wrapper = shallow(<Controls />);
-    expect(wrapper.find('h3')).to.have.length(1);
+  it('should have div tag', () => {
+    const wrapper = shallow(<Controls store={testStore} />);
+    expect(wrapper.find('div')).to.have.length(1);
+  });
+});
+
+describe('<Challenges />', () => {
+  it('should have a state', () => {
+    const wrapper = shallow(<Challenges store={testStore} />);
+    expect(wrapper.state()).to.be.defined;
+    expect(wrapper.state('solution') === 'abc').to.equal(true);
   });
 });
 
 describe('<Challenge />', () => {
-  const wrapper = shallow(<Challenge />);
-  it('should have a state', () => {
-    expect(wrapper.state()).to.be.defined;
-    expect(wrapper.state('solution') === 'abc').to.equal(true);
+  it('should have props challengeInfo', () => {
+    const wrapper = shallow(<Challenge store={testStore} />);
+    expect(wrapper.props().challengeInfo).to.be.defined;
+  });
+});
+
+describe('<InputRegexValidation />', () => {
+  it('checkRegex should be defined', () => {
+    const wrapper = shallow(<InputRegexValidation store={testStore} />);
+    expect(wrapper.checkRegex).to.be.defined;
   });
 
-  it('should have a checkRegex function', () => {
+  it('checkRegex should be a function', () => {
+    const wrapper = shallow(<InputRegexValidation store={testStore} />);
     expect(wrapper.instance().checkRegex).to.be.a('function');
   });
 });
 
-describe('<Challenge />', () => {
-  const wrapper = shallow(<Challenge />);
-  it('should have a state', () =>{
-    expect(wrapper.state()).to.be.defined;
-  });
 
-  it('should have a checkRegex function', () => {
-    expect(wrapper.checkRegex).to.be.defined;
-  });
-});
+// Invariant Violation: Could not find "store" in either the context or props of.
+// Either wrap the root component in a <Provider>, or explicitly pass "store" as a prop to
