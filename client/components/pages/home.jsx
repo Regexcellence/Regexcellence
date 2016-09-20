d589a34d7ba1621ef999b1a47ef4f5251fb94965
hellodanali <danagotmail@gmail.com>
2016-09-20 16:06:08 -0700

    [BUGFIX] Fix linting errors

--- client/components/pages/home.jsx
+++ client/components/pages/home.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 
 class Home extends Component {
-	render() {
-		return (<h1>This is the home page</h1>);
-	}
+  render() {
+    return (<h1>This is the home page</h1>);
+  }
 }
 
 export default Home;
--- client/components/pages/tutorial.jsx
+++ client/components/pages/tutorial.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 
 class Tutorial extends Component {
-	render() {
-		return (<h1>This is the Tutorial page</h1>);
-	}
+  render() {
+    return (<h1>This is the Tutorial page</h1>);
+  }
 }
 
-export default Tutorial
+export default Tutorial;
