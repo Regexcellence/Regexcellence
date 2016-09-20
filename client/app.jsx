96f35e563d5958d26ce9c271f32c41478ffcf0e6
lwonsower <lwonsower@gmail.com>
2016-09-20 13:39:53 -0700

    [ FEATURE ] add preliminary client-side routing

--- client/app.jsx
+++ client/app.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { Router, browseHistory } from 'react-router';
 // For creating eventual <Provider /> tag
 import { Provider } from 'react-redux';
 // To create 'store' variable
@@ -8,6 +9,7 @@ import reducer from './reducers/index';
 
 import Controls from './controls';
 import thunk from 'redux-thunk';
+import routes from './routes.jsx';
 
 const finalStore = applyMiddleware(thunk)(createStore);
 const store = finalStore(reducer);
@@ -17,7 +19,9 @@ class App extends React.Component {
     return (
       <div>
         <p>Hello Regexcellence</p>
-        <Provider store={store}>
+          <Router routes={routes} history={browseHistory} />
+          <div>Hello {this.props.children} </div>
+          <Provider store={store}>
           <Controls />
         </Provider>
       </div>
---
+++ client/components/pages/tutorial.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Tutorial extends Component {
+	render() {
+		return (<h1>This is the Tutorial page</h1>);
+	}
+}
+
+export default Tutorial
