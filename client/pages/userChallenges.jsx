eb5d86db3a6799f8c836e5df5b69fe6532fa371a
hellodanali <danagotmail@gmail.com>
2016-09-22 14:16:29 -0700

    [ FEATURE ] Add userChallenger folder underr /client

---
+++ client/pages/userChallenges.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import Navigation from '../controls/navbar';
+import ListView from '../userChallenges/listView';
+import { getAllChallenges } from '../actions/index';
+
+class UserChallenges extends Component {
+  componentWillMount() {
+    this.props.getAllChallenges();
+  }
+  render() {
+    if(this.props.challenges.length){
+      console.log('***user challenges: ', this.props.challenges);
+    }
+    return (
+      <div>
+        <Navigation />
+        <h1>User Challenges Page</h1>
+        <ListView />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return { challenges: state.challenges };
+};
+
+export default connect(mapStateToProps, { getAllChallenges })(UserChallenges);
+
+
