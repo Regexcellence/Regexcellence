8c83162af4d13c104c2f556d0f5c79f0064bb102
lwonsower <lwonsower@gmail.com>
2016-09-24 16:05:21 -0700

    [ FEATURE ] style css

--- client/tutorials/progressBar.jsx
+++ client/tutorials/progressBar.jsx
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 
 class ProgressBar extends Component {
   render() {
-    console.log("in progbar", this.props.progress)
     return(
       <div className="outerProgress" title="Progress">
+      <div className="text"><b>{this.props.progress.width}</b></div>
         <div className="innerProgress" style={this.props.progress}>
         </div>
       </div>
