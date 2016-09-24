b21d1ce62b9481203bcf6742c1402311bd7e6930
lwonsower <lwonsower@gmail.com>
2016-09-24 10:56:45 -0700

    [ FEATURE ] config progress bar

--- client/tutorials/tutorialMain.jsx
+++ client/tutorials/tutorialMain.jsx
@@ -41,6 +41,10 @@ class TutorialMain extends Component {
       }
       return (
         <div className="clear-top">
+          <div class="progress">
+            <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width:70%">70%
+            </div>
+          </div>
           <Challenge
             key={currentTutorial._id}
             challengeInfo={currentTutorial}
