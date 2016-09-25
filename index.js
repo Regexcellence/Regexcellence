b974c97fb3d56dcac62200dcd061aab76dfba9ae
Benjamin Tran <benjamin.tran25@gmail.com>
2016-09-25 16:42:57 -0700

    [ CLEANUP ] Refactor, lint, and cleanup index file

--- index.js
+++ index.js
@@ -1,28 +1,13 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const session = require('express-session');
-const GithubStrategy = require('passport-github2').Strategy;
-const passport = require('passport');
-const gitHubAuth = require('./server/github-auth/auth');
-const User = require('./server/db/dbmodel').Users;
 
 const app = express();
-
-app.use(session({ secret: 'keyboard cat',
-  resave: true,
-  saveUninitialized: true,
-  cookie: { maxAge: 300000 },
-}));
-app.use(passport.initialize());
-app.use(passport.session());
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
-const handleRequest = require('./server/handlers');
-handleRequest(app);
 
 const mongoose = require('mongoose');
 const MONGO_URI = require('./config').MONGO_URI;
@@ -36,28 +21,13 @@ if (TARGET !== 'devStart') {
   app.use(express.static(path.join(process.env.PWD, 'build')));
 }
 
-// serialize and deserialize
-passport.serializeUser((user, done) => {
-  console.log('serializeUser: ', user.accessToken);
-  done(null, user);
-});
-passport.deserializeUser((id, done) => {
-  User.findById(id, (err, user) => {
-    console.log('INSIDE DESERIALZE! USER DATTA: ', user);
-      if(!err) done(null, user);
-      else done(err, null);
-    });
-});
+const gitAuth = require('./server/github-auth/git-auth-handlers');
 
-app.get('/auth/github',
-passport.authenticate('github', { scope: ['user:email'] }));
+gitAuth(app);
 
-app.get('/auth/github/callback',
-passport.authenticate('github', { failureRedirect: '/' }),
-(req, res) => {
-  console.log('SESSION:', req.session);
-  res.redirect('/#');
-});
+const handleRequest = require('./server/handlers');
+
+handleRequest(app);
 
 mongoose.connect(MONGO_URI);
 app.listen(port, () => { console.log(`Listening on http://localhost:${port}/`); });
