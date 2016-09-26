b5a51ede5434e96f8dee8dd46e7c5128dc8ee345
Troy Gibb <troy.gibb@gmail.com>
2016-09-26 16:09:22 -0700

    [ FEATURE ] Begin to add post authentication

--- client/challenge/regexInput/inputRegexValidation.jsx
+++ client/challenge/regexInput/inputRegexValidation.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { testRegexInput, inputActionCreator } from '../../actions/index';
 import InputValidationFlag from './inputValidationFlag';
 import TestPassedButton from './testPassedButton';
+import PostAuthentication from './postAuthentication';
 
 class InputRegexValidation extends React.Component {
   constructor(props) {
@@ -11,25 +12,35 @@ class InputRegexValidation extends React.Component {
     this.addSlashes = this.addSlashes.bind(this);
   }
   render() {
-    return (
-      <div>
-        <form id="input-pattern" className="input-group">
-          <input
-            className="form-control"
-            placeholder="enter your pattern here..."
-            value={this.props.input}
-            onChange={this.changeInputState}
-            onFocus={this.addSlashes}
-          />
-          <TestPassedButton
-            nextUrl={this.props.nextUrl}
-            editable={this.props.editable}
-            testPassed={this.props.testPassed}
-          />
-        </form>
-        <InputValidationFlag wellFormedInput={this.props.wellFormedInput} />
-      </div>
-    );
+    const { editable } = this.props;
+    const { validInput } = this.props.newUserPost;
+    if (!editable || validInput) {
+      return (
+        <div>
+          <form id="input-pattern" className="input-group">
+            <input
+              className="form-control"
+              placeholder="enter your pattern here..."
+              value={this.props.input}
+              onChange={this.changeInputState}
+              onFocus={this.addSlashes}
+            />
+            <TestPassedButton
+              nextUrl={this.props.nextUrl}
+              editable={this.props.editable}
+              testPassed={this.props.testPassed}
+            />
+          </form>
+          <InputValidationFlag wellFormedInput={this.props.wellFormedInput} />
+        </div>
+      );
+    } else {
+      return <PostAuthentication 
+        newUserPost={this.props.newUserPost} 
+        id="input-pattern" 
+        className="input-group"
+        />
+    }
   }
   addSlashes(event) {
     if (!event.target.value.length) {
@@ -45,7 +56,11 @@ class InputRegexValidation extends React.Component {
 }
 
 const mapStateToProps = (state) => {
-  return { input: state.userInput, wellFormedInput: state.wellFormedInput };
+  return { 
+    input: state.userInput, 
+    wellFormedInput: state.wellFormedInput,
+    newUserPost: state.newUserPost
+  };
 };
 
 export default connect(mapStateToProps, { testRegexInput, inputActionCreator })(InputRegexValidation);
---
+++ client/challenge/regexInput/postAuthentication.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+export default class PostAuthentication extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <div>YO!</div>
+  }
+}
