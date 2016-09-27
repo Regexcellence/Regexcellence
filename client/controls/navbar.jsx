08a7feba4aad66b8d35e3f21180971158e04f222
Troy Gibb <troy.gibb@gmail.com>
2016-09-26 18:28:32 -0700

    [ BUGFIX ] Rename logIn to login

--- client/controls/navbar.jsx
+++ client/controls/navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router';
 
 import Cheatsheet from '../pages/cheatsheet';
 import Footer from '../controls/footer';
-import Login from '../login/logIn';
+import Login from '../login/login';
 
 export default class Navbar extends Component {
   constructor(props) {
