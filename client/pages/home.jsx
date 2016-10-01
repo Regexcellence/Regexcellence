73ea3918985129d1c8b5e8cd75954559896d7e88
lwonsower <lwonsower@gmail.com>
2016-10-01 16:27:11 -0700

    [ FEATURE ] expand content on home page

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -29,23 +29,25 @@ class Home extends React.Component {
             <div className="row homepage-row">
               <div className="col-md-4 home-images">
                 <div className="book-logo"><span className="book-r">R</span><img className="list-img" src={book} /></div><br />
-                <h3><Link to="tutorial">LEARN</Link></h3>
+                <h3 className="learn-h3"><Link to="tutorial">LEARN</Link></h3>
                 <div className="image-text">Take our comprehensive tutorial to learn the basics of Regex!</div>
               </div>
                 <div className="col-md-4 home-images">
                   <img className="list-img" src={arrows} /><br /><br />
-                  <h3><Link to="user-challenges">CHALLENGE</Link></h3>
+                  <h3 className="challenge-h3"><Link to="user-challenges">CHALLENGE</Link></h3>
                   <div className="image-text">Practice your new skills at our user submitted Regex challenges.</div>
                 </div>
               <div className="col-md-4 home-images">
                 <img className="quill" src={quill} /><br /><br />
-                <h3><Link to="post">CONTRIBUTE</Link></h3>
+                <h3 className="contribute-h3"><Link to="post">CONTRIBUTE</Link></h3>
                 <div className="image-text">Become part of the community by submitting your own Regex challenges.</div>
               </div>
             </div>
         </div>
         <div className="lower-home">
-          <p>Regular Expressions have been around since the 50s, but it wasn’t until the 80s that the regex we know and love today really started to develop. A lot of the patterns and syntax was designed specifically to work with Perl, and you will still sometimes hear the term Perl-style thrown around. Luckily regex works well with a myriad of languages today, and honing your skills will help you become an all-around phenomenal programer!</p>
+          <p>Regular Expressions have been around since the 50s, but it wasn’t until the 80s that the regex we know and love today really started to develop. A lot of the patterns and syntax was designed specifically to work with Perl, and you will still sometimes hear the term Perl-style thrown around. Luckily regex works well with a myriad of languages today, and honing your skills will help you become an all-around phenomenal programer!</p><br /><span className="glyphicon glyphicon-grain" /><br /><br />
+          <p>Regex is used to locate, match, and manage text. Learning how to use it has the potential to save you thousands of hours of work. Regexcellence was developed to teach you the basic formation of these fantastic patterns and provide a platform for you to practice writing them.
+          </p>
         </div>
       </div>
     );
