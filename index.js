9210d5f1e33ffb74555b5ca406839e19e19cdb9c
Troy Gibb <troy.gibb@gmail.com>
2016-10-01 08:23:25 -0700

    [ CLEANUP ] Take out comments

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
-// parse application/json
 app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
