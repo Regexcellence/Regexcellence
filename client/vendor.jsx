cfa10767d379b3242aa14e9046eaca421a5e7471
Benjamin Tran <benjamin.tran25@gmail.com>
2016-10-02 23:51:11 -0700

    [ BUFGIX ] Replace XRegexP with xregexp to resolve path on vendor.jsx

--- client/vendor.jsx
+++ client/vendor.jsx
@@ -2,8 +2,8 @@
 
 import React from 'react';
 import ReactRedux from 'react-redux';
-import ReactRouter from 'react-router';	
+import ReactRouter from 'react-router';
 import ReactDOM from 'react-dom';
 import ReduxThunk from 'redux-thunk';
 import uuid from 'uuid';
-import XRegExp from 'XRegExp';
+import XRegExp from 'xregexp';
