ff06ff591b33fe54d692c8ab44376d69945df8c9
Troy Gibb <troy.gibb@gmail.com>
2016-10-03 10:17:55 -0700

    [ BUGFIX, FEATURE ] Add tutorial answers | Bugfix for challenge list view in profile

--- client/user-profile/profile.jsx
+++ client/user-profile/profile.jsx
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import mapChallenges from './profile-challenge-lists';
 
 import { getUserCompletedChallenges } from '../actions/api';
+import ListItem from '../userChallenges/listItem';
+
 
 class UserProfile extends React.Component {
   constructor(props) {
@@ -12,6 +14,20 @@ class UserProfile extends React.Component {
     if (this.props.userInfo._id) {
       this.props.getUserCompletedChallenges(this.props.userInfo._id);
     }
+  }
+  mapChallenges(array) {
+    return listContent.map((each, i) => {
+      console.log('CONTENTS!!!!', listContent);
+      return (
+        <ListItem
+          key={i}
+          challengeId={each._id}
+          name={each.name}
+          difficulty={each.difficulty}
+          testCases={null}
+        />
+      );
+    });
   }
   render() {
     const { userInfo } = this.props;
@@ -28,10 +44,10 @@ class UserProfile extends React.Component {
       );
     } else {
       if (typeof completed_challenges[0] === 'object') {
-        completeLists = mapChallenges(completed_challenges);
+        completeLists = this.mapChallenges(completed_challenges);
       }
       if (typeof authored_challenges[0] === 'object') {
-        authoredList = mapChallenges(authored_challenges);
+        authoredList = this.mapChallenges(authored_challenges);
       }
       return (
         <div className="user-profile">
