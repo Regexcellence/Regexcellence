3e71fbf89fe57c87adcfbc483a24c636a5f07f3a
lwonsower <lwonsower@gmail.com>
2016-10-06 13:41:58 -0700

    [ FEATURE ] donut hover complete

---
+++ client/user-profile/toolTip.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class ToolTip extends Component {
+  render() {
+  	let position = {
+  		top: this.props.top + "px",
+  		left: this.props.left + "px",
+  	}
+  	return (
+      <div className="tool-tip" style={position}>
+        {this.props.value} completed {this.props.keyVal} 
+        {this.props.value === 1 ? " challenge" : " challenges"}
+      </div>
+  	)
+  }
+}
