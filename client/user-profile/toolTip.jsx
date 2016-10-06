90e34bbc119eb2633ca627aced264bf2abb717f8
lwonsower <lwonsower@gmail.com>
2016-10-06 14:33:06 -0700

    [ BUGFIX ] fix dominant flickering on donut hover

--- client/user-profile/toolTip.jsx
+++ client/user-profile/toolTip.jsx
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 export default class ToolTip extends Component {
   render() {
   	let position = {
-  		top: this.props.top + "px",
-  		left: this.props.left + "px",
+  		top: (this.props.top+20) + "px",
+  		left: (this.props.left+20) + "px",
   	}
   	console.log(position)
   	return (
